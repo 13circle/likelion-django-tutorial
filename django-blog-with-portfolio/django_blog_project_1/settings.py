617a201ebe649b644d73d452ee41fb2ca70104a3
13circle <13circle97@gmail.com>
2020-07-13 18:25:07 +0900

    Apply Django Template Inheritance
    base.html now has whole template of the blog.

--- django-blog-with-portfolio/django_blog_project_1/settings.py
+++ django-blog-with-portfolio/django_blog_project_1/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_blog_project_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['django_blog_project_1/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
